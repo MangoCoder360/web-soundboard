9cc04d5cf042f55ba475203cb7142e5a42c78aa5
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2025-11-18 18:40:16 -0800

    add ui sounds and advertisements

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function getMoreGyros() {
         document.getElementById('gyros-amount').innerText = gyros;
         document.title = 'you got ' + randomAmount + ' gyros!!!!!';
     }
+
+    playSteamUiSound('deck_ui_positive_confirmation');
 }
 
 function pullNewBoosterPack() {
@@ -60,13 +62,37 @@ function updateInventoryDisplay() {
     }
 }
 
+var adInterval;
 function openSlotsUi() {
-    document.getElementById('slots-ui').contentDocument.location.reload(true);
-    setTimeout(() => {
-        document.getElementById('slots-ui-container').style.display = 'flex';
-    }, 80);
+    var shouldShowAd = Math.random() < 0.5; // 50% chance to show ad
 
+    var adVideo = document.getElementById('ad-video');
     document.title = 'gambling addict';
+
+    document.getElementById('slots-ui').contentDocument.location.reload(true);
+
+    if (shouldShowAd) {
+        document.getElementById('ad').style.display = 'block';
+        adVideo.currentTime = 0;
+        adVideo.play();
+        adVideo.playbackRate = 2;
+
+        setTimeout(() => {
+            document.getElementById('slots-ui-container').style.display = 'flex';
+            document.getElementById('ad').style.display = 'none';
+            clearInterval(adInterval);
+        }, adVideo.duration * 500 + 1000);
+
+        adInterval = setInterval(() => {
+            // display seconds remaining in the #ad-header
+            var timeRemaining = Math.ceil((adVideo.duration - adVideo.currentTime) / 3);
+            document.getElementById('ad-header').innerText = 'UNSKIPPABLE AD - ' + timeRemaining + 's remaining';
+        }, 100);
+    } else {
+        document.getElementById('slots-ui-container').style.display = 'flex';
+        playSteamUiSound('deck_ui_show_modal');
+        return;
+    }
 }
 
 function closeSlotsUi() {
@@ -74,4 +100,11 @@ function closeSlotsUi() {
     init();
 
     document.title = 'gyros game';
+    playSteamUiSound('deck_ui_hide_modal');
+}
+
+function playSteamUiSound(name) {
+    var audio = document.getElementById('ui-sfx-player');
+    audio.src = 'https://cdn.reuben.zip/steam-ui-sfx/' + name + '.wav';
+    audio.play();
 }
